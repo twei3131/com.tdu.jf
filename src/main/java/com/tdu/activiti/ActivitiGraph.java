058b8e83b80897690c4abee7d50d569a6c8ec69b
twei3131 <848270978@qq.com>
2019-05-30 19:25:37 +0800

    9th

--- src/main/java/com/tdu/activiti/ActivitiGraph.java
+++ src/main/java/com/tdu/activiti/ActivitiGraph.java
@@ -2,9 +2,11 @@ package com.tdu.activiti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.apache.log4j.Logger;
 
 public class ActivitiGraph extends ActivitiGraphUtils
 {
+	Logger logger = Logger.getLogger(ActivitiGraph.class);
     public ActivitiGraph()
     {
 		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
@@ -13,5 +15,7 @@ public class ActivitiGraph extends ActivitiGraphUtils
 		.name("请假")
 		.addClasspathResource("/com/tdu/processes/vacate.bpmn")
 		.deploy();
+        logger.info("部署完毕");
+        System.out.println("部署完毕");
     }
 }
