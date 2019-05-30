71dd21effe94da7f93e59e58ecee5b0c6e828353
twei3131 <848270978@qq.com>
2019-05-30 19:58:21 +0800

    10th

--- src/main/java/com/tdu/controller/UserController.java
+++ src/main/java/com/tdu/controller/UserController.java
@@ -1,19 +1,25 @@
 package com.tdu.controller;
 
+import com.google.gson.Gson;
 import com.jfinal.core.Controller;
 import com.tdu.model.User;
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.history.HistoricProcessInstance;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserController extends Controller
 {
+    Log logger = LogFactory.getLog(UserController.class);
     public void index()
     {
         User user = User.dao.findById(25);
@@ -30,13 +36,19 @@ public class UserController extends Controller
     {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey("apply");
+        HistoryService historyService = processEngine.getHistoryService();
+        List<HistoricProcessInstance> list = historyService.createHistoricProcessInstanceQuery().orderByProcessInstanceStartTime()
+                .asc()//排序
+                .list();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
         Task vacationTask = processEngine.getTaskService().createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
         processEngine.getTaskService().setAssignee(vacationTask.getId(), "1");
         Map<String, Object> map = new HashMap<>();
         map.put("days", 3);
         map.put("formId", 1);
         processEngine.getTaskService().complete(vacationTask.getId(),map);
-        renderText("Done!");
+        renderText("Done! " + json);
     }
 
 }
